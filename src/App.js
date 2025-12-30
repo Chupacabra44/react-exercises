e8f0c156027c4dfb7b463f57220876c98acb8ebd
Sanja Daic <sanja.daic86@gmail.com>
2025-12-30 10:38:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = ["Hello React", "React is a Framework", "React Best"];
+
+const App = () => {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handlePre = () => {
+    if (step > 1) {
+      return setStep(step - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (step < 3) {
+      return setStep(step + 1);
+    }
+  };
+
+  const handleOpen = () => {
+    if (isOpen) {
+      setIsOpen(false);
+    } else {
+      setStep(1);
+      setIsOpen(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handleOpen}>{isOpen ? "Hide" : "Show"}</button>
+      {isOpen && (
+        <div className="wrapper">
+          <div className="numberWrapper">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p>
+            {step}:{messages[step - 1]}
+          </p>
+          <div className="btnGroup">
+            <button onClick={handlePre}>Previous</button>
+            <button onClick={handleNext}>Next</button>
+          </div>
+        </div>
+      )}
+    </>
   );
-}
+};
 
 export default App;
